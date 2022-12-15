ffb638a24a4ce1cbde318e51effdafc0cffc7347
RobinRattasep <rattasep.robin@voco.ee>
2022-12-15 05:42:07 -0800

    Registering form

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const sequelize = require('./database');
 const pug = require('pug');
 app.set('view engine', 'pug');
 app.use(express.static('public'))
+const bodyParser = require('body-parser')
 
-
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 
 
@@ -33,12 +36,27 @@ app.get('/register', (req, res) => {
         res.render('register');
 });
 
-
-app.get('/hotel', (req, res) => {
-    res.render('hotel');
+app.get('/clientregister', (req, res) => {
+    res.render('clientregister');
 });
 
 
+
+app.get('/hotel/:id', async (req, res) => {
+    try {
+        const andmed = await sequelize.query('SELECT * FROM hotell', { type: sequelize.QueryTypes.SELECT });
+        res.render('hotel', {andmed});
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'An error occurred while trying to select * from hotellid' });
+    }
+});
+
+app.post('/registering', (req, res) => {
+    console.log("Tere")
+    console.log(req.body)
+});
 
 
 app.listen(3000, () => {
