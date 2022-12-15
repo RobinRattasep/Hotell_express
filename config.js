7969270c65b975308fd297b8d5c9601a35727f4b
RobinRattasep <rattasep.robin@voco.ee>
2022-12-15 03:50:51 -0800

    Added database connection and hotels to the main page [#184040938]

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: 'ikt.khk.ee:2083',
-    user: 'syttroometiktkhk',
-    password: 'Kylaline!',
-    database: 'syttroometiktkhk_hotell'
+    host: '54.38.198.24',
+    user: 'u1_5LBuN26Baq',
+    password: 'Wypdnm5UkinzUmCTYUMOmB^.',
+    database: 's1_hotell'
 };
--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const app = express();
 const sequelize = require('./database');
+const pug = require('pug');
+app.set('view engine', 'pug');
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
 
-app.get('/users', async (req, res) => {
+
+
+
+
+app.get('/styles.css', (req, res) => {
+    res.type('text/css')
+    res.sendFile('styles.css', { root: __dirname })
+})
+
+app.get('/', async (req, res) => {
     try {
-        const users = await sequelize.query('SELECT * FROM kasutaja', { type: sequelize.QueryTypes.SELECT });
-        res.json(users);
+        const hotell = await sequelize.query('SELECT * FROM hotell', { type: sequelize.QueryTypes.SELECT });
+        console.log(hotell)
+        res.render('index', {hotell});
+
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'An error occurred' });
+        res.status(500).json({ error: 'An error occurred while trying to select * from hotellid' });
     }
 });
+
+
+app.get('/register', (req, res) => {
+        res.render('register');
+});
+
+
+app.get('/hotel', (req, res) => {
+    res.render('hotel');
+});
+
+
 
 
 app.listen(3000, () => {
